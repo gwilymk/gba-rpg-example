d8a1af4dced8582db082617dee9a63a8637da31c
Gwilym Kuiper <gw@ilym.me>
2020-05-20 22:20:37 +0100

    Use volatile memory in TileMap.c

--- lostgba/src/TileMap.c
+++ lostgba/src/TileMap.c
@@ -1,32 +1,33 @@
 #include <lostgba/TileMap.h>
+#include "LostGbaInternal.h"
 
 #include <string.h>
 
-#define SPRITE_PALETTE_MEMORY_LOCATION ((u16 *)0x05000200)
+#define SPRITE_PALETTE_MEMORY_LOCATION ((vu16 *)0x05000200)
 
 void TileMap_CopyToSpritePalette(const u16 paletteData[TileMap_PaletteLength])
 {
-    memcpy(SPRITE_PALETTE_MEMORY_LOCATION, paletteData, TileMap_PaletteLength * sizeof(u16));
+    LostGBA_VMemCpy32(SPRITE_PALETTE_MEMORY_LOCATION, paletteData, TileMap_PaletteLength * sizeof(u16));
 }
 
-#define SPRITE_CHARBLOCK_BASE ((u16 *)0x06010000)
+#define SPRITE_CHARBLOCK_BASE ((vu16 *)0x06010000)
 #define CHARBLOCK_SIZE 0x4000
 
 void LOSTGBA_UNSAFE(TileMap_CopyToSpriteTiles)(int tileNumber, const u32 *tileData, int length)
 {
-    memcpy(SPRITE_CHARBLOCK_BASE + tileNumber * CHARBLOCK_SIZE, tileData, length);
+    LostGBA_VMemCpy32(SPRITE_CHARBLOCK_BASE + tileNumber * CHARBLOCK_SIZE, tileData, length);
 }
 
-#define BG_PALETTE_MEMORY_LOCATION ((u16 *)0x05000000)
+#define BG_PALETTE_MEMORY_LOCATION ((vu16 *)0x05000000)
 
 void TileMap_CopyToBackgroundPalette(const u16 paletteData[TileMap_PaletteLength])
 {
-    memcpy(BG_PALETTE_MEMORY_LOCATION, paletteData, TileMap_PaletteLength * sizeof(u16));
+    LostGBA_VMemCpy32(BG_PALETTE_MEMORY_LOCATION, paletteData, TileMap_PaletteLength * sizeof(u16));
 }
 
-#define TILE_MEMORY_LOCATION ((u16 *)0x06000000)
+#define TILE_MEMORY_LOCATION ((vu16 *)0x06000000)
 
 void LOSTGBA_UNSAFE(TileMap_CopyToBackgroundTiles)(int tileNumber, const u32 *tileData, int length)
 {
-    memcpy(TILE_MEMORY_LOCATION + tileNumber * CHARBLOCK_SIZE, tileData, length);
+    LostGBA_VMemCpy32(TILE_MEMORY_LOCATION + tileNumber * CHARBLOCK_SIZE, tileData, length);
 }
