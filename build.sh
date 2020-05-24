0329c9b2896bb7824f54ad61195a2815ae51b295
Gwilym Kuiper <gw@ilym.me>
2020-05-24 18:41:14 +0100

    Force user to run sudo ./build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sudo docker build -t gba-dev .
-sudo docker run --network="host" --rm --volume $PWD:/build -it --workdir "/build" gba-dev make "$@"
+docker build -t gba-dev .
+docker run --network="host" --rm --volume $PWD:/build -it --workdir "/build" gba-dev make "$@"
