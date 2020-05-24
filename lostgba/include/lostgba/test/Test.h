8685df78b2808c5ce1f61450a71c36c9cfa0e593
Gwilym Kuiper <gw@ilym.me>
2020-05-24 12:56:10 +0100

    Fix up test function definition

--- lostgba/include/lostgba/test/Test.h
+++ lostgba/include/lostgba/test/Test.h
@@ -1,12 +1,16 @@
 #pragma once
 
-#define LostGBA_Test(testName)                                                       \
-    static void LostGBA_Test_##__LINE__(const char *LostGBA_TestName);               \
-    __attribute__((constructor)) static void LostGBA_Construct_Test_##__LINE__(void) \
-    {                                                                                \
-        LostGBA_TestRegister(&LostGBA_Test_##__LINE__, testName);                    \
-    }                                                                                \
-    static void LostGBA_Test_##__LINE__(const char *LostGBA_TestName)
+#define LostGBA_Concat2__(a, b) a##b
+#define LostGBA_Concat__(a, b) LostGBA_Concat2__(a, b)
+#define LostGBA_TestName__ LostGBA_Concat__(LostGBA_Test_, __LINE__)
+
+#define LostGBA_Test(testName)                                                                            \
+    static void LostGBA_TestName__(const char *LostGBA_TestName);                                         \
+    __attribute__((constructor)) static void LostGBA_Concat__(LostGBA_Register, LostGBA_TestName__)(void) \
+    {                                                                                                     \
+        LostGBA_TestRegister(&LostGBA_TestName__, testName);                                              \
+    }                                                                                                     \
+    static void LostGBA_TestName__(const char *LostGBA_TestName)
 
 typedef void (*LostGBA_TestMethod)(const char *LostGBA_TestName);
 
