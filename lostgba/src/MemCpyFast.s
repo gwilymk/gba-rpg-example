ab138bb0e8317433925c1638e0bd8db6cfd534f8
Gwilym Kuiper <gw@ilym.me>
2020-05-25 12:11:42 +0100

    TODONE

--- lostgba/src/MemCpyFast.s
+++ lostgba/src/MemCpyFast.s
@@ -62,7 +62,6 @@ LostGBA_VMemCpy32_Fast:
 @ Will use the fast 32-bit version above internally but this handles non-word length (but not unaligned memory)
 @ Both source and target must be word aligned, and length must be a multiple of 2 (half-word aligned)
 @
-@ TODO: Handle the case where source and target aren't word aligned
 @ TODO: Handle the case where source xor target aren't word aligned
 @ TODO: Handle the case where bytes % 2 != 0
 .section .iwram, "ax", %progbits @ "ax" = allocatable and executable, %progbits = contains data
